e8006fc229a19be7705520f0e461e77ef978e729
Niklas Baudy <niklas.baudy@vanniktech.de>
2016-03-17 20:23:13 +0100

    Fix bug when only parent class had @OnActivityResult annotation

--- onactivityresult/src/test/java/onactivityresult/ActivityResultTest.java
+++ onactivityresult/src/test/java/onactivityresult/ActivityResultTest.java
@@ -20,12 +20,12 @@ public class ActivityResultTest {
     @Rule public final ExpectedException expectedException = ExpectedException.none();
 
     @Test
-    public void testConstructorShouldBePrivate() {
+    public void constructorShouldBePrivate() {
         PrivateConstructorChecker.forClass(ActivityResult.class).expectedTypeOfException(AssertionError.class).expectedExceptionMessage("No instances.").check();
     }
 
     @Test
-    public void testOnActivityResultClassIsNotFound() {
+    public void onActivityResultClassIsNotFound() {
         final ActivityResult.OnResult onResult = ActivityResult.onResult(0, 0, null);
         final LonelyObject lonelyObject = new LonelyObject();
 
@@ -37,7 +37,7 @@ public class ActivityResultTest {
     public static class LonelyObject {}
 
     @Test
-    public void testOnActivityResultClassFound() {
+    public void onActivityResultClassFound() {
         final ActivityResult.OnResult onResult = ActivityResult.onResult(1, 2, null);
         final OnResult result = new OnResult();
 
@@ -56,7 +56,7 @@ public class ActivityResultTest {
     }
 
     @Test
-    public void testOnResultShouldThrowRuntimeExceptionWithPrivateConstructorTextWhenOnActivityResultHasPrivateConstructor() {
+    public void onResultShouldThrowRuntimeExceptionWithPrivateConstructorTextWhenOnActivityResultHasPrivateConstructor() {
         final ActivityResult.OnResult onResult = ActivityResult.onResult(0, 0, null);
         final OnResultPrivateObject onResultPrivateObject = new OnResultPrivateObject();
 
@@ -75,4 +75,25 @@ public class ActivityResultTest {
             throw new RuntimeException("Extract was called");
         }
     }
+
+    @Test
+    public void childActivityResultWillLookForParentWhenChildHasNoGeneratedClass() {
+        final ActivityResult.OnResult onResult = ActivityResult.onResult(1, 2, null);
+        final Child child = new Child();
+
+        expectedException.expect(RuntimeException.class);
+        expectedException.expectMessage("Extract was called in parent");
+        onResult.into(child);
+    }
+
+    public static class Parent {}
+
+    public static final class Parent$$OnActivityResult implements IOnActivityResult<Parent> {
+        @Override
+        public boolean onResult(final Parent extractingObjectTimeProvider, final int requestCode, final int resultCode, final Intent intent) {
+            throw new RuntimeException("Extract was called in parent");
+        }
+    }
+
+    public static class Child extends Parent {}
 }
