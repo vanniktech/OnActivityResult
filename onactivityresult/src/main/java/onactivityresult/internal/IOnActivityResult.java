2cea1717bde9679ef7c57921aa0656598db399f5
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-08-12 10:00:41 +0200

    Better Travis Configuration + dependency updates.

--- onactivityresult/src/main/java/onactivityresult/internal/IOnActivityResult.java
+++ onactivityresult/src/main/java/onactivityresult/internal/IOnActivityResult.java
@@ -3,5 +3,5 @@ package onactivityresult.internal;
 import android.content.Intent;
 
 public interface IOnActivityResult<T> {
-    boolean onResult(final T t, final int requestCode, final int resultCode, final Intent intent);
+    boolean onResult(T t, int requestCode, int resultCode, Intent intent);
 }
